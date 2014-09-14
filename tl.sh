e1afdf2f6d1057be07c3d68ea90c4aeb9595c65b
Manuel Bonk <m@bonk.io>
2014-09-14 22:50:45 +0200

    modified:   tl.sh

--- tl.sh
+++ tl.sh
@@ -3,25 +3,18 @@
 path="/srv/nfs/cam"
 
 re='^[0-9]+$'
-if ! [[ $1 =~ "$re" ]];  then
-	echo -e "usage: tl.sh [interval between photos]\nor:    tl.sh [interval between photos] [output directory]"
-	echo $path
-	echo $1
-	echo $2
+if  ! [[ $1 =~ "$re" ]];  then
+	echo -e "usage: tl.sh [interval between photos]\n   or: tl.sh [interval between photos] [output directory]"
 	exit
 fi
 
-if [[ -z "$2"]]; then
-	path="$2"
+if ! [[ -z "$2" ]]; then
+	path=$2
 fi
 
-echo $path
+mkdir $path
 
-if [ ! -d "$path" ]; then
-	mkdir -pv $path
-fi
-#while true; do
-#	./raspistill -rot 180 -v -o /srv/nfs/cam/`date +"%Y-%m-%d_%H.%M.%S.jpg"`
-#	sleep $1
-#done
-
+while true; do
+	./raspistill -rot 180 -v -o $path/`date +"%Y-%m-%d_%H.%M.%S.jpg"`
+	sleep $1
+done
